74d81816ff6297f05a418fcfbbdacd7c1bd90790
BloodWolf10 <126118338+BloodWolf10@users.noreply.github.com>
2025-05-02 15:16:30 -0500

    Registration Validation

--- app/src/main/java/com/wizinc/renteasyjm/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/wizinc/renteasyjm/viewmodel/RegisterViewModel.kt
@@ -4,10 +4,16 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.wizinc.renteasyjm.data.User
+import com.wizinc.renteasyjm.util.RegisterFieldState
+import com.wizinc.renteasyjm.util.RegisterValidation
 import com.wizinc.renteasyjm.util.Resource
+import com.wizinc.renteasyjm.util.validateEmail
+import com.wizinc.renteasyjm.util.validatePassword
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
@@ -19,21 +25,48 @@ class RegisterViewModel @Inject constructor (
     private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Unspecified())
     val register: Flow<Resource<FirebaseUser>> = _register
 
+    private val _validation = Channel<RegisterFieldState> ()
+    val validation = _validation.receiveAsFlow()
+
     fun createAccountWithEmailAndPassword(user: User, password: String) {
-        runBlocking {
-            _register.emit(Resource.Loading())
-        }
-        firebaseAuth.createUserWithEmailAndPassword(user.email, password)
-            .addOnSuccessListener {
-                it.user?.let{
-                    _register.value = Resource.Success(it)
+
+        if (checkValidation(user, password)) {
+
+
+            runBlocking {
+                _register.emit(Resource.Loading())
+            }
+            firebaseAuth.createUserWithEmailAndPassword(user.email, password)
+                .addOnSuccessListener {
+                    it.user?.let {
+                        _register.value = Resource.Success(it)
+                    }
                 }
+
+                .addOnFailureListener {
+                    _register.value = Resource.Error(it.message.toString())
+                }
+
+        } else {
+            val registerFieldState = RegisterFieldState(
+                validateEmail(user.email), validatePassword(password)
+            )
+
+            runBlocking {
+                _validation.send(registerFieldState)
             }
+        }
 
-            .addOnFailureListener {
-            _register.value = Resource.Error(it.message.toString())
-            }
+    }
+
+    private fun checkValidation(user: User, password: String): Boolean {
+        val emailValidation = validateEmail(user.email)
+        val passwordValidation = validatePassword(password)
+        val shouldRegister =
+            emailValidation is RegisterValidation.Success &&
+                    passwordValidation is RegisterValidation.Success
 
 
+        return shouldRegister
     }
 }
