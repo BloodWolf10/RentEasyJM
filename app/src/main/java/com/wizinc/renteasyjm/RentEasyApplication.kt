493892a898072b3cf0ed4ddc7ab7c8744926b916
BloodWolf10 <126118338+BloodWolf10@users.noreply.github.com>
2025-05-02 14:21:07 -0500

    Register and Firebase Setup

---
+++ app/src/main/java/com/wizinc/renteasyjm/RentEasyApplication.kt
@@ -0,0 +1,8 @@
+package com.wizinc.renteasyjm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RentEasyApplication: Application() {
+}
--- app/src/main/java/com/wizinc/renteasyjm/fragments/loginregister/RegisterFragment.kt
+++ app/src/main/java/com/wizinc/renteasyjm/fragments/loginregister/RegisterFragment.kt
@@ -1,9 +1,74 @@
 package com.wizinc.renteasyjm.fragments.loginregister
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.wizinc.renteasyjm.R
+import com.wizinc.renteasyjm.data.User
+import com.wizinc.renteasyjm.databinding.FragmentRegisterBinding
+import com.wizinc.renteasyjm.util.Resource
+import com.wizinc.renteasyjm.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class RegisterFragment: Fragment(R.layout.fragment_register){
+private val TAG = "RegisterFragment"
+
+@AndroidEntryPoint
+class RegisterFragment: Fragment(){
+
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<RegisterViewModel>()
 
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRegisterBinding.inflate(inflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            buttonRegisterRegister.setOnClickListener{
+                val user = User(
+                    edFirstName.text.toString().trim(),
+                    edLastName.text.toString().trim(),
+                    edEmailRegister.text.toString().trim(),
+
+                )
+                val password = edPassowrdRegister.text.toString()
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect{
+                when(it){
+                    is Resource.Loading ->{
+                        binding.buttonRegisterRegister.startAnimation()
+                    }
+                    is Resource.Success ->{
+                      Log.d("test", it.data.toString())
+                        binding.buttonRegisterRegister.revertAnimation()
+                    }
+                    is Resource.Error -> {
+                        Log.e(TAG,it.message.toString())
+                        binding.buttonRegisterRegister.revertAnimation()
+
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
+    }
 }
